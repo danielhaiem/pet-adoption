98d73e8f7a7751e2c80613b1be5380a8a37def37
Daniel Haiem <dnhaiem@gmail.com>
2022-09-23 15:41:06 +0300

    refactored user middleware

--- middleware/userMiddleware.ts
+++ middleware/userMiddleware.ts
@@ -1,11 +1,11 @@
-import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import { User } from '../models/userModel';
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel";
 
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   const { token } = req.cookies;
   if (!token) {
-    res.status(401).send('Token Required');
+    res.status(401).send("Token Required");
     return;
   }
   jwt.verify(
@@ -13,10 +13,11 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     process.env.TOKEN_SECRET as string,
     (err: any, decoded: any) => {
       if (err) {
-        res.status(401).send('Invalid Token');
+        res.status(401).send("Invalid Token");
         return;
       }
       req.body.userId = decoded.id;
+      req.body.isAdmin = decoded.isAdmin;
       next();
     }
   );
@@ -24,12 +25,12 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
 
 const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
   const user = await User.findById(req.body.userId, { isAdmin: 1 });
-  if (user.isAdmin === true) {
+  if (user.isAdmin === true && req.body.isAdmin === true) {
     req.body.user = user;
     next();
     return;
   }
-  res.status(400).send('Only Admins can access this page.');
+  res.status(400).send("Only Admins can access this page.");
 };
 
 export { verifyToken, isAdmin };
